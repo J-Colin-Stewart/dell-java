32bcb0db4f06ee3e67f84a8b57fadbf2c3916016
J-Colin-Stewart <giest13@gmail.com>
2019-01-09 18:47:14 -0600

    Close, but not quite working. Submitting to show progress.

--- RockPaperScissors/src/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -4,62 +4,92 @@ import java.util.Scanner;
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
-		// Introduction.
-		System.out.println("Let's play Rock, Paper Scissors.");
-		System.out.println("Press 0 for Rock, 1 for Paper, 2 for Scissors:");
-		// Call method to get and display player choice.
-		String PlayerChoice = getPlayerChoice();
-		
-		public static String getPlayerChoice() {
-			Scanner reader = new Scanner(System.in);
-			String playerRPS = reader.next();
-			if (playerRPS.equalsIgnoreCase("rock")){
-				System.out.println("You chose Rock.");
-			}
-			if (playerRPS.equalsIgnoreCase("paper")){
-				System.out.println("You chose Paper.");
-			}
-			if (playerRPS.equalsIgnoreCase("scissors")){
-				System.out.println("You chose Scissors.");
-			}
-			return playerRPS;
-		}
-		
-		// Randomly choose a hand for computer.
-		Random randNumGenerator = new Random();
-		int computerRPS = randNumGenerator.nextInt(3);
-		if (computerRPS == 0) {
-	    System.out.println("Computer chose Rock.");
-	  }
-	  if (computerRPS == 1) {
-	    System.out.println("Computer chose Paper.");
-	  }
-	  if (computerRPS == 2) {
-	    System.out.println("Computer chose Scissors.");
-	  }
-	  // Print result.
-	  if (playerRPS == computerRPS) {
-	  	System.out.println("It's a tie!");
-	  }
-	  if ((playerRPS == 0) && (computerRPS == 1)) {
-	  	System.out.println("Paper covers Rock. Computer wins!");
-	  }
-	  if ((playerRPS == 1) && (computerRPS == 2)) {
-	  	System.out.println("Scissors cut Paper. Computer wins!");
-	  }
-	  if ((playerRPS == 2) && (computerRPS == 0)) {
-	  	System.out.println("Rock smashes Scissors. Computer wins!");
-	  }
-	  if ((playerRPS == 0) && (computerRPS == 2)) {
-	  	System.out.println("Rock smashes Scissors. Player wins!");
-	  }
-	  if ((playerRPS == 1) && (computerRPS == 0)) {
-	  	System.out.println("Paper covers Rock. Player wins!");
-	  }
-	  if ((playerRPS == 2) && (computerRPS == 1)) {
-	  	System.out.println("Scissors cut Paper. Player wins!");
-	  }
-	  reader.close();
+		// Introduction, set up for a nerd joke.
+		System.out.println("SHALL WE PLAY A GAME?");
+		System.out.println("I only play Rock, Paper Scissors.");
+		System.out.print("So... Rock, paper, or scissors? ");
+		letsPlay();
 	}
-
+	
+	// Method containing gameplay.
+	public static void letsPlay() {
+		String playerChoice = getPlayerChoice();
+		System.out.println("You chose: " + playerChoice);
+		String computerChoice = getComputerChoice();
+		System.out.println("Computer chose: " + computerChoice);
+		String winMessage = getWinner(playerChoice, computerChoice);
+		System.out.print(winMessage);
+	}
+		
+	// Get player choice and return it.
+	public static String getPlayerChoice() {
+		Scanner reader = new Scanner(System.in);
+		String selection = reader.next();
+		if (selection.equalsIgnoreCase("rock") || selection.equalsIgnoreCase("paper") || selection.equalsIgnoreCase("scissors")){
+			String playerRPS = selection;
+		} 
+		if (selection.equalsIgnoreCase("global thermonuclear war")) {
+			System.out.println("A STRANGE GAME.");
+			System.out.println("THE ONLY WINNING MOVE IS");
+			System.out.println("NOT TO PLAY.");
+			System.out.println("HOW ABOUT A NICE GAME OF ROCK, PAPER, SCISSORS?");
+			getPlayerChoice();
+		} 
+		else {
+			System.out.println("Not a valid choice. Please try again.");
+			getPlayerChoice();
+		}
+		return playerRPS;
+		reader.close();
+		
+	}
+		
+	// Randomly choose a hand for computer and return it as a string.
+	public static String getComputerChoice() {
+		Random randNumGenerator = new Random();
+		int i = randNumGenerator.nextInt(3);
+		String computerRPS = "";
+		if (i == 0) {
+		   computerRPS = "Rock";
+		 }
+		 if (i == 1) {
+		   computerRPS = "Paper";
+		 }
+		 if (i == 2) {
+		   computerRPS = "Scissors";
+		 }
+		 return computerRPS;
+	}
+	
+	// Compare choices and declare a winner.
+	public static String getWinner(String playerChoice, String computerChoice) {
+		if (playerChoice.equalsIgnoreCase("rock") && computerChoice.equalsIgnoreCase("paper")) {
+			System.out.println("Paper covers Rock. ");
+			return ("Computer wins!");
+		}
+		if (playerChoice.equalsIgnoreCase("paper") && computerChoice.equalsIgnoreCase("scissors")) {
+			System.out.println("Scissors cut Paper. ");
+			return ("Computer wins!");
+		}
+		if (playerChoice.equalsIgnoreCase("scissors") && computerChoice.equalsIgnoreCase("rock")) {
+			System.out.println("Rock smashes Scissors. ");
+			return ("Computer wins!");
+		}
+		if (playerChoice.equalsIgnoreCase("rock") && computerChoice.equalsIgnoreCase("scissors")) {
+			System.out.println("Rock smashes Scissors. ");
+			
+		}
+		if (playerChoice.equalsIgnoreCase("paper") && computerChoice.equalsIgnoreCase("rock")) {
+			System.out.println("Paper covers Rock. ");
+			return ("Player wins!");
+		}
+		if (playerChoice.equalsIgnoreCase("scissors") && computerChoice.equalsIgnoreCase("paper")) {
+			System.out.println("Scissors cut Paper. ");
+			return ("Player wins!");
+		}
+		else {
+			return("It's a tie!");
+		}
+	}
 }
+
